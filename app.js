7c479586b39827d05245b14d15e4e5fc48022b70
pDunCanCode <chesiredarling@gmail.com>
2021-03-25 09:34:37 -0700

    correct app.js 'listen' error && indentation errors

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const port = 3003;
 const middleware = require("./middleware");
 const path = require("path");
 
-const server = app.prependOnceListener(port, () =>
-  console.log("Listening on port " + port)
-);
+const server = app.listen(port, () => console.log("Listening on port " + port));
 
 app.set("view engine", "pug");
 app.set("views", "views");
