17d7de550c94043082695cf4dbcbd68fc326ad0a
pDunCanCode <chesiredarling@gmail.com>
2021-03-29 09:06:05 -0700

    add client side vaidation checks

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -14,9 +14,21 @@ router.get("/", (req, res, next) => {
 
 router.post("/", (req, res, next) => {
 
-  console.log(req.body);
+  let firstName = req.body.firstName.trim();
+  let lastName = req.body.lastName.trim();
+  let username = req.body.username.trim();
+  let email = req.body.email.trim();
+  let password = req.body.password;
+  let payload = req.body;
+  if (firstName && lastName && username && email && password) {
+
+
+  } else {
+    payload.errorMessage = "Make sure each field has a valid value";
+    res.status(200).render("register", payload);
+  }
+
   
-  res.status(200).render("register");
 });
 
 module.exports = router;
