817e683724a7ce26a4f44316bdb64884a9d221f7
pDunCanCode <chesiredarling@gmail.com>
2021-04-08 21:29:42 -0700

    add to loginRoutes

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
 const User = require("../schemas/UserSchema");
+const bcrypt = require("bcrypt");
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -21,7 +22,17 @@ router.post("/", async (req, res, next) => {
       payload.errorMessage = "Something went wrong.";
       res.status(200).render("login", payload);
     });
+    if (user != null) {
+      let result = await bcrypt.compare(req.body.logPassword, user.password);
+      if (result === true) {
+        req.session.user = user;
+        return res.redirect("/");
+      }
+    }
+    payload.errorMessage = "Login credentials incorrect.";
+    res.status(200).render("login", payload);
   }
+  payload.errorMessage = "Make sure each field has a valid value";
   res.status(200).render("login");
 });
 
