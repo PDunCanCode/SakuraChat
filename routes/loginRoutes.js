cbe7c58c613c3355b3bcdc7af06f58c45226f5d7
pDunCanCode <chesiredarling@gmail.com>
2021-04-08 21:21:14 -0700

    add router.post fnunction to loginRoutes

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,13 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const router = express.Router();
+const bodyParser = require("body-parser");
+const User = require("../schemas/UserSchema");
 
 app.set("view engine", "pug");
 app.set("views", "views");
 
+app.use(bodyParser.urlencoded({ extended: false }));
 router.get("/", (req, res, next) => {
-    
-    res.status(200).render("login");
-})
+  res.status(200).render("login");
+});
+router.post("/", async (req, res, next) => {
+  let payload = req.body;
+  if (req.body.logUsername && req.body.logPassword) {
+    let user = await User.findOne({
+      $or: [{ username: username }, { email: email }],
+    }).catch((error) => {
+      console.log(error);
+      payload.errorMessage = "Something went wrong.";
+      res.status(200).render("login", payload);
+    });
+  }
+  res.status(200).render("login");
+});
 
 module.exports = router;
