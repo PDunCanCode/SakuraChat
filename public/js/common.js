f7fb20e79636165826bacd626ff129815bcb0dc4
pDunCanCode <chesiredarling@gmail.com>
2021-04-16 09:01:20 -0700

    submitPost function

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,12 @@
 $("#postTextArea").keyup((event) => {
   let textbox = $(event.target);
   let value = textbox.val().trim();
-  console.log(value);
+  let submitButton = $("#submitPostButton");
+
+  if (submitButton.length == 0) return alert("No submit Button found");
+  if (value == "") {
+    submitButton.prop("disabled", true);
+    return;
+  }
+  submitButton.prop("disabled", false);
 });
