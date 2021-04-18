ab0ccf72e2da9fd513f095637fc7ad4557c18b30
pDunCanCode <chesiredarling@gmail.com>
2021-04-18 11:14:36 -0700

    add res.staus to posts.js

--- routes/api/posts .js
+++ routes/api/posts .js
@@ -6,6 +6,12 @@ const User = require("../schemas/UserSchema");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 router.get("/", (req, res, next) => {});
-router.post("/", async (req, res, next) => {});
+router.post("/", async (req, res, next) => {
+  if (!req.body.content) {
+    console.log("Content not sent with request");
+    return res.status(400);
+  }
+  res.status(200).send("it Worked!");
+});
 
 module.exports = router;
