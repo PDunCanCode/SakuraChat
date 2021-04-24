5dce8f444440c6d32029042b4fdc339d1819fd23
pDunCanCode <chesiredarling@gmail.com>
2021-04-24 09:18:22 -0700

    add createPostHtml function

--- public/js/common.js
+++ public/js/common.js
@@ -24,5 +24,22 @@ $("#submitPostButton").click(() => {
   });
 });
 function createPostHtml(postData) {
-  return postData.content;
+  return `<div class='post'>
+
+              <div class='mainContainer'>
+                <div class='userImageContainer'>
+                  <img src='${postedBy.profilePic}'>
+                </div>
+                <div class='postContentContainer'>
+                  <div class='header'>
+                  </div>
+                  <div class='postBody'>
+                    <span>${postData.content}</span>
+                  </div>
+                  <div class='postFooter'>
+                  </div>
+                </div>
+              </div>
+
+          /</div>;`;
 }
